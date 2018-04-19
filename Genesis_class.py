1d6f1dfe095e65d62bd12a0f61cd0eeab2c3ff54
Codehana <33840240+Codehana@users.noreply.github.com>
2018-04-19 19:53:42 +0200

    Add files via upload

--- Genesis_class.py
+++ Genesis_class.py
@@ -3,31 +3,28 @@
 import random
 
 class Genesis:
-
-	def __init__(self, nohy = None, kozich = None, jidlo = None):
+	def __init__(self, nohy = 4, kozich = None, jidlo = None):
 		self.nohy = nohy
 		self.kozich = kozich
 		self.jidlo = jidlo
 
 	def dej_nohy(self):
-		koncetiny = ["2","3","4","6"]
+		koncetiny = [2,3,4,6]
 		self.nohy = random.choice(koncetiny)
 		return self.nohy
 	def dej_kozich(self):
-		kozisek = ["True","False"]
+		kozisek = [True,False]
 		self.kozich = random.choice(kozisek)
 		return self.kozich
 	def dej_jidlo(self):
 		papani = ["maso","vegie","oboje"]
 		self.jidlo = random.choice(papani)
-		return self.jidlo
 
 def udelej_zvirata():
 	z = int(input("Kolik má být zvířat? "))
 	zvirata = []
 	x = Genesis()
-	while z != 0:
-		z = z - 1
+	for z in range(z):
 		zvirata.append({"nohy": x.dej_nohy(), "kozich": x.dej_kozich(), "jidlo": x.dej_jidlo()})
 	return zvirata
 
